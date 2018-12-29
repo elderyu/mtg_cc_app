434f0fe7d5517ea363722204329c9be926857bf8
KW <werniokrzysztof@gmail.com>
2018-12-29 11:18:11 +0100

    123

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,35 +6,19 @@ class CardsController < ApplicationController
   end
 
   def find
-    url_base = "https://api.scryfall.com/cards"
-    url_extra = ''
     if params[:random] == '1'
-      url_extra = "/random"
-      url = url_base + url_extra
-      buffer = open(url).read
-      result = JSON.parse(buffer)
-      @res_name = result["name"]
-      @res_image = result["image_uris"]["normal"]
-      card = Card.new(title: result["name"], image_url: result["image_uris"]["normal"])
-      card.save
+      request_get_and_save "/random"
       render 'search'
     else
       if params[:cards][:card_name].blank?
         flash.now[:danger] = "No card name entered."
         render 'search'
       else
-        url_extra = "/named?exact=#{params[:cards][:card_name]}"
-        url = url_base + url_extra
         begin
-          buffer = open(url).read
-          result = JSON.parse(buffer)
-          @res_name = result["name"]
-          @res_image = result["image_uris"]["normal"]
-          card = Card.new(title: result["name"], image_url: result["image_uris"]["normal"])
-          card.save
+          request_get_and_save "/named?exact=#{params[:cards][:card_name]}"
           render 'search'
         rescue OpenURI::HTTPError => e
-          flash.now[:danger] = "Card not found" if e.message == "404 Not Found"
+          flash.now[:danger] = "Card not found." if e.message == "404 Not Found"
           render 'search'
         end
       end
@@ -46,18 +30,22 @@ class CardsController < ApplicationController
   end
 
   def add
-    begin
-      Rails::logger.debug "%%%%"
-      Rails::logger.debug params[:cards][:count]
-      Rails::logger.debug "%%%%"
-      collected_card = Card.find_by(title: params[:title])
-      # Rails::logger.debug collected_card
-      current_user.add_card collected_card
-      current_user.collected_cards.where(card_id: collected_card.id).update(count: params[:cards][:count])
-      render 'cards/search'
-    rescue Exception => e
-      flash.now[:danger] = e.message
-      render 'cards/search'
+    if params[:cards][:count]
+      # errors.add "Number of cards was not specified."
+      flash.now[:danger] = "Number of cards was not specified."
+      request_get_and_save "/named?exact=#{params[:title]}"
+      render 'search'
+    else
+      begin
+        collected_card = Card.find_by(title: params[:title])
+        current_user.add_card collected_card
+        current_user.collected_cards.where(card_id: collected_card.id).update(count: params[:cards][:count])
+        flash.now[:success] = "#{params["title"]} successfully added to your collection! Number of copies: #{params[:cards][:count]}"
+        render 'search'
+      rescue Exception => e
+        flash.now[:danger] = e.message
+        render 'search'
+      end
     end
   end
 
@@ -65,6 +53,15 @@ class CardsController < ApplicationController
 
     def permitted_params
       params.permit(:user_id, :card_id, :title, :cards["count"])
+    end
+
+    def request_get_and_save url_extra=''
+      url_base = "https://api.scryfall.com/cards"
+      url = url_base + url_extra
+      buffer = open(url).read
+      @result = JSON.parse(buffer)
+      card = Card.new(title: @result["name"], image_url: @result["image_uris"]["normal"])
+      card.save
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ resources :users
   post 'add', to: 'cards#add'
   get 'add', to: 'cards#search'
 
+  resources :decks
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
