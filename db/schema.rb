a79bb7c933ffc3eab8d8e08c24924473aac931eb
KW <werniokrzysztof@gmail.com>
2019-01-15 12:31:45 +0100

    1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_171634) do
+ActiveRecord::Schema.define(version: 2019_01_15_112705) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_01_07_171634) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "mana_cost", default: "--- []\n"
+    t.string "card_id"
   end
 
   create_table "collected_cards", force: :cascade do |t|
